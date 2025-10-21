26b87deb1befd85a167a95a7f872c74c373e9b15
maxence30 <henous.maxence@outlook.fr>
2025-10-21 12:55:56 +0000

    dzdz

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet - affiche l'alphabet en minuscules suivi d'un saut de ligne
+ * print_alphabet - prints the lowercase alphabet followed by a new line
  */
 void print_alphabet(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-		_putchar(c);  /* 1er appel à _putchar */
-
-	_putchar('\n');    /* 2ème appel à _putchar */
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }

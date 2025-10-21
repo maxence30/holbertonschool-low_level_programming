db4ac4afb49920f98aa9fbb73e10486fb8b289f7
maxence30 <henous.maxence@outlook.fr>
2025-10-21 13:17:35 +0000

    rfrg

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - prints "_putchar" followed by a new line
  *
  * Return: Always 0 (Success)
  */
@@ -11,11 +11,5 @@ int main(void)
     _putchar('p');
     _putchar('u');
     _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-
-    return (0);
-}
+    _p_
+	

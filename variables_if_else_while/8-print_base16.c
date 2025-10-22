4c63b706bde1a601bb49dc966ffa2a89976cd9cd
maxence30 <henous.maxence@outlook.fr>
2025-10-22 08:43:58 +0000

    okles100pourcent

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - affiche les nombres base 16 (hexadécimal) en minuscules suivis d'un saut de ligne
+ * main - affiche les nombres base 16 (hexadécimal) en minuscules
+ * suivis d'un saut de ligne
  *
  * Return: 0 (succès)
  */
@@ -12,12 +13,11 @@ int main(void)
 	for (i = 0; i < 16; i++)
 	{
 		if (i < 10)
-			putchar(i + '0');          /* 1ère utilisation de putchar */
+			putchar(i + '0');
 		else
-			putchar(i - 10 + 'a');     /* 2ème utilisation de putchar */
+			putchar(i - 10 + 'a');
 	}
 
-	putchar('\n');                       /* 3ème utilisation de putchar */
-
+	putchar('\n');
 	return (0);
 }

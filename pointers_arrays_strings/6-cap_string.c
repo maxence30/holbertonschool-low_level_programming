c48ee483b03bba573ecf4e5a26ff87176f6651a0
maxence30 <henous.maxence@outlook.fr>
2025-10-29 08:25:38 +0000

    th

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string to modify
+ *
+ * Return: pointer to the modified string
+ */
+char *cap_string(char *str)
+{
+	int i, j;
+	char sep[] = " \t\n,;.!?\"(){}";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 && (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] = str[i] - 32;
+		}
+
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (str[i] == sep[j] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+			{
+				str[i + 1] = str[i + 1] - 32;
+				break;
+			}
+		}
+	}
+
+	return (str);
+}

e551df04330c68987654a6bb013cfdddf844079c
maxence30 <henous.maxence@outlook.fr>
2025-10-29 08:20:41 +0000

    ok

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,51 +1,20 @@
-Write a function that reverses the content of an array of integers.
-
-Prototype: void reverse_array(int *a, int n);
-Where n is the number of elements of the array
-julien@ubuntu:~/$ cat 4-main.c
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_array - print an array of integers
- * @a: an array of integers
- * @n: the number of elements to swap
+ * reverse_array - reverses the content of an array of integers
+ * @a: array of integers
+ * @n: number of elements in the array
  *
- * Return: nothing.
+ * Return: nothing
  */
-void print_array(int *a, int n)
+void reverse_array(int *a, int n)
 {
-    int i;
+    int i, temp;
 
-    i = 0;
-    while (i < n)
+    for (i = 0; i < n / 2; i++)
     {
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-        i++;
+        temp = a[i];
+        a[i] = a[n - 1 - i];
+        a[n - 1 - i] = temp;
     }
-    printf("\n");
 }
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
-
-    print_array(a, sizeof(a) / sizeof(int));
-    reverse_array(a, sizeof(a) / sizeof(int));
-    print_array(a, sizeof(a) / sizeof(int));
-    return (0);
-}
-julien@ubuntu:~/$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 4-main.c 4-rev_array.c -o 4-rev_array
-julien@ubuntu:~/$ ./4-rev_array 
-0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337
-1337, 1024, 98, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
-julien@ubuntu:~/$ 

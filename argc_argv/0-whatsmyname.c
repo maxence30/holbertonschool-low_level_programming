4eaaa1889bb203e54f8e2a44121a80811fabab74
maxence30 <henous.maxence@outlook.fr>
2025-11-07 22:59:23 +0000

    x

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program name followed by a new line
+ * @argc: number of command-line arguments
+ * @argv: array of command-line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

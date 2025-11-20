8fc2f4bcf5de34e8d5c52db563dd9793264a5754
maxence30 <henous.maxence@outlook.fr>
2025-11-20 10:05:06 +0000

    a

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure representing a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */

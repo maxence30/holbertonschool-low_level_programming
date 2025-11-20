75ee1e6d206bd5ee2f6667f7e6f5011393665908
maxence30 <henous.maxence@outlook.fr>
2025-11-20 08:31:26 +0000

    a

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -4,14 +4,14 @@
 /**
  * print_name - prints a name using a function pointer
  * @name: the name to print
- * @f: function pointer to the printing function
+ * @f: pointer to a function that takes a char pointer
  *
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name == NULL || f == NULL)
-        return;
+	if (name == NULL || f == NULL)
+		return;
 
-    f(name);
+	f(name);
 }

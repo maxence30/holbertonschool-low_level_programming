3819a065101e9d3d0ac740b2572457ed3585fe88
maxence30 <henous.maxence@outlook.fr>
2025-11-20 10:14:18 +0000

    a

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,6 +16,9 @@ struct dog
 	char *owner;
 };
 
+/* Alias dog_t pour struct dog */
+typedef struct dog dog_t;
+
 /* Prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);

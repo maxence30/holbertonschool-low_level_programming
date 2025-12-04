933738cccafe1dc4cf3f51efc0cecaeebc2e9442
maxence30 <henos.maxence@outlook.fr>
2025-12-04 20:46:54 +0000

    cbo,nuykyuk-

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -4,71 +4,53 @@
 
 /**
  * main - keygen for crackme5
- * @argc: number of args
- * @argv: args
+ * @argc: number of arguments
+ * @argv: arguments
  *
- * Return: 0
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-    unsigned int i, len;
-    unsigned long ch;
-    char *username;
-    char keygen[7];
-    unsigned int add, mult;
-    const char lookup[] =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/.";
+	char *username;
+	char key[7];
+	int len, i, tmp;
+	char *table = "A-CHRDw87lNS0E9B2TibgpnMVyu7j3fxzKXUPt5LDJrsQaZHIkWqomYF4Cev09+";
 
-    if (argc != 2)
-    {
-        printf("Usage: %s username\n", argv[0]);
-        return (1);
-    }
+	if (argc != 2)
+		return (1);
 
-    username = argv[1];
-    len = strlen(username);
+	username = argv[1];
+	len = strlen(username);
 
-    /* keygen[0] */
-    keygen[0] = (len ^ 59) & 63;
+	key[0] = table[len ^ 59];
 
-    /* keygen[1] */
-    add = 0;
-    for (i = 0; i < len; i++)
-        add += (unsigned char)username[i];
-    keygen[1] = (add ^ 79) & 63;
+	tmp = 0;
+	for (i = 0; i < len; i++)
+		tmp += username[i];
+	key[1] = table[(tmp ^ 79) & 63];
 
-    /* keygen[2] */
-    mult = 1;
-    for (i = 0; i < len; i++)
-        mult *= (unsigned char)username[i];
-    keygen[2] = (mult ^ 85) & 63;
+	tmp = 1;
+	for (i = 0; i < len; i++)
+		tmp *= username[i];
+	key[2] = table[(tmp ^ 85) & 63];
 
-    /* keygen[3] */
-    ch = (unsigned char)username[0];
-    for (i = 0; i < len; i++)
-        if ((unsigned long)(unsigned char)username[i] > ch)
-            ch = (unsigned long)(unsigned char)username[i];
-    srand(ch ^ 14);
-    keygen[3] = rand() & 63;
+	tmp = username[0];
+	for (i = 0; i < len; i++)
+		if (username[i] > tmp)
+			tmp = username[i];
+	srand(tmp ^ 14);
+	key[3] = table[rand() & 63];
 
-    /* keygen[4] */
-    ch = 0;
-    for (i = 0; i < len; i++)
-        ch += ((unsigned char)username[i] * (unsigned char)username[i]);
-    keygen[4] = (ch ^ 239) & 63;
+	tmp = 0;
+	for (i = 0; i < len; i++)
+		tmp += username[i] * username[i];
+	key[4] = table[(tmp ^ 239) & 63];
 
-    /* keygen[5] */
-    for (i = 0; i < (unsigned char)username[0]; i++)
-        srand(rand());
-    keygen[5] = (rand() ^ 229) & 63;
+	srand(username[0] ^ 229);
+	key[5] = table[rand() & 63];
 
-    keygen[6] = '\0';
+	key[6] = '\0';
+	printf("%s", key);
 
-    /* convert to final characters using lookup table */
-    for (i = 0; i < 6; i++)
-        keygen[i] = lookup[(unsigned char)keygen[i]];
-
-    printf("%s", keygen);
-
-    return (0);
+	return (0);
 }
